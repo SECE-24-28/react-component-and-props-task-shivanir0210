f5f849734e920be54c9c1613ac6f54dfaaf37371
shivanir0210 <shivanirasappan@gmail.com>
2026-06-09 09:08:24 +0530

    js basics

---
+++ basics/basics1.js
@@ -0,0 +1,66 @@
+//let a=10;
+// var a =14;
+// a=12;
+
+// function add(a,b){
+//     return a+b;
+// }
+// console.log(add(4,5));
+
+// const add = (a, b) => {
+//     return a + b;
+// };
+
+// console.log(add(4, 5));
+
+// let a ="hello"
+// // console.log("hi "+(a));
+// console.log(`hi ${a}`);
+
+//array destructure
+// const ans =["hello","hi","hlo"];
+// console.log(ans[1]);
+
+// const arr = ["hiiiiii", "hi", "hlo"];
+// const[abi,hi,hlo] =arr;
+// console.log(hlo);
+
+// const student ={
+//     name:"xxx",
+//     age:19
+// };
+
+// console.log(student.name);
+
+// const arr = ["xxx", 19];
+// const [name, age] = arr;
+// console.log(name); // abi
+// console.log(age);  // 19
+
+//spread operator
+// const a =[1,2];
+// const b =[...a];
+// console.log(b);
+
+// const a =[1,2,3]
+// const b= [4,5,6]
+// const c=[...a,...b]
+// console.log(c);
+
+// //rest operator
+// const add = (...num)=>{
+//     console.log(num);
+// }
+// add(1,2,3,4,5,6,7,8);
+
+//map
+// const a=[1,2,3,4,5]
+// const r = a.map((n) => n*2);
+// console.log(r);
+
+//filter
+// const a=[1,2,3,4,5,6,7,8]
+// const b= a.filter((n) => n%2==0)
+// const c= a.filter((n) => n%2!=0)
+// console.log(b);
+// console.log(c);
